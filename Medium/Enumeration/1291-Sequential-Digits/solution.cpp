c0f4d166831e64d1903c9dacd8f71318bbe181f3
G. Thrishanth Reddy <thrishanthreddygoli@gmail.com>
2026-07-26 14:40:11 +0530

    add code

--- Medium/Enumeration/1291-Sequential-Digits/solution.cpp
+++ Medium/Enumeration/1291-Sequential-Digits/solution.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        
+        vector<int> ans;
+        string s = "123456789";
+
+        for (int len = 2; len <= 9; len++) {
+            for (int start = 0; start + len <= 9; start++) {
+
+                int num = 0;
+                for (int i = start; i < start + len; i++) {
+                    num = num * 10 + (s[i] - '0');
+                }
+
+                if (num >= low && num <= high)
+                    ans.push_back(num);
+            }
+        }
+
+        return ans;
     }
 };
