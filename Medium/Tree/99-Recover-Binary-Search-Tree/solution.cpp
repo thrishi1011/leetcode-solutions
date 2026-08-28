19e1ac3eb28346ed5c7750f8205fcbe0701728fb
G. Thrishanth Reddy <thrishanthreddygoli@gmail.com>
2026-08-28 16:41:42 +0530

    Add Recover Binary Search Tree (Medium)

---
+++ Medium/Tree/99-Recover-Binary-Search-Tree/solution.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* first = nullptr;
+    TreeNode* second = nullptr;
+    TreeNode* prev = new TreeNode(INT_MIN);
+
+    void inorder(TreeNode* root) {
+        if (!root) return;
+
+        inorder(root->left);
+
+        if (prev && root->val < prev->val) {
+            if (!first) first = prev;
+            second = root;
+        }
+        prev = root;
+
+        inorder(root->right);
+    }
+
+    void recoverTree(TreeNode* root) {
+        inorder(root);
+        int temp = first->val;
+        first->val = second->val;
+        second->val = temp;
+    }
+};
